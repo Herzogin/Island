43bd56ee7851d5fae1c5f8b6440208986b740821
Herzogin <s0550233@htw-berlin.de>
2020-10-10 23:08:48 +0200

    first commit

---
+++ Assets/Scripts/GateEnter.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GateEnter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("gate enter loaded");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //print("inside OnTrigger");
+        //SceneManager.LoadScene("SampleScene");
+
+        if (other.tag == "Player")
+        {
+            SceneManager.LoadScene("SampleScene");
+
+        }
+    }
+
+    //private void SwitchScene(int sceneNumber)
+    //{
+    //    SceneManager.LoadScene("SampleScene");
+    //}
+}
---
+++ Assets/Scripts/ObjectMovesRandomly.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMovesRandomly : MonoBehaviour
+{
+    private float movementDuration = 2.0f;
+    private float waitBeforeMoving = 2.0f;
+    private bool hasArrived = false;
+
+    private void Update()
+    {
+        if (!hasArrived)
+        {
+            hasArrived = true;
+            float randX = Random.Range(230.0f, 260.0f); //(-5.0f, 5.0f);
+            float randY = Random.Range(36.5f, 38.0f); //(-5.0f, 5.0f);
+            float randZ = Random.Range(225.0f, 250.0f);
+            StartCoroutine(MoveToPoint(new Vector3(randX, randY, randZ)));
+        }
+    }
+
+    private IEnumerator MoveToPoint(Vector3 targetPos)
+    {
+        movementDuration = Random.Range(5.0f, 10.0f); //= 2.0f;
+        waitBeforeMoving = Random.Range(2.0f, 4.0f); //= 2.0f;
+        float timer = 0.0f;
+        Vector3 startPos = transform.position;
+
+        while (timer < movementDuration)
+        {
+            timer += Time.deltaTime;
+            float t = timer / movementDuration;
+            t = t * t * t * (t * (6f * t - 15f) + 10f);
+            transform.position = Vector3.Lerp(startPos, targetPos, t);
+
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(waitBeforeMoving);
+        hasArrived = false;
+    }
+}
