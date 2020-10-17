2e43c90c41dabe94b75d60ee4cc00e7e4b2aec27
Herzogin <s0550233@htw-berlin.de>
2020-10-17 18:52:25 +0200

    second platform moves

--- Assets/Scripts/CharacterStaysOnObject.cs
+++ Assets/Scripts/CharacterStaysOnObject.cs
@@ -6,12 +6,20 @@ public class CharacterStaysOnObject : MonoBehaviour
 {
 
     public GameObject Player;
+    [SerializeField] private Animator animatorPlatform;
+     
 
+    void Start()
+    {
+        animatorPlatform = GetComponent<Animator>();
+        
+    }
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
             Player.transform.parent = transform;
+            animatorPlatform.SetBool("CharacterOnPlatform", true);
         }
     }
 
@@ -20,9 +28,11 @@ public class CharacterStaysOnObject : MonoBehaviour
         if (other.gameObject.tag == "Player")
         {
             Player.transform.parent = null;
+            animatorPlatform.SetBool("CharacterOnPlatform", false);
         }
     }
 
+   
 
 }
 
