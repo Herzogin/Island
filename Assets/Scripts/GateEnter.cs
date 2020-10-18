f9f16fbc2a1a1add08419cd08f6e448c512ba2b3
Herzogin <s0550233@htw-berlin.de>
2020-10-18 15:39:48 +0200

    three moving platforms lead to gate

--- Assets/Scripts/GateEnter.cs
+++ Assets/Scripts/GateEnter.cs
@@ -7,7 +7,6 @@ using UnityEngine.SceneManagement;
 
 public class GateEnter : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         print("gate enter loaded");
